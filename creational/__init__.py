f386868e317c8cbee68c28a374f2c68813c48f9c
HeberTU <heber.trujillo@glovoapp.com>
2022-08-05 15:26:59 +0200

    feat: factory method init.

---
+++ creational/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Creational design patterns.
+
+These patterns provide various object creation mechanisms, which increase
+flexibility and reuse of existing code.
+
+Created on: 5/8/22
+@author: Heber Trujillo <heber.trj.urt@gmail.com>
+Licence,
+"""
---
+++ creational/factory_method/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Factory method module.
+
+Is a creational design pattern that provides an interface for creating objects
+in a superclass, but allows subclasses to alter the type of objects that will
+be created.
+
+Created on: 5/8/22
+@author: Heber Trujillo <heber.trj.urt@gmail.com>
+Licence,
+"""
